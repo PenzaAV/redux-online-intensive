2ad5fa59d7bd0d991a71fd29275f856be5de34a6
Anton Penza <anton_p@evnedev.com>
2019-02-11 19:34:34 +0200

    rename branch

--- source/bus/posts/saga/workers/removePost.js
+++ source/bus/posts/saga/workers/removePost.js
@@ -11,7 +11,6 @@ export function* removePost ({ payload: postId }) {
         yield put(uiActions.startFetching());
         const response = yield apply(api, api.posts.remove, [postId]);
         if (response.status !== 204) {
-
             throw new Error(message);
         }
         yield put(postsActions.removePost(postId));
