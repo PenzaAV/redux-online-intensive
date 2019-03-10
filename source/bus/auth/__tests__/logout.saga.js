61e6433ab4b844f10774391ecc3c0f1f94ef133f
Anton Penza <penzaav@gmail.com>
2019-03-10 15:45:20 +0200

    lesson 13 homework

---
+++ source/bus/auth/__tests__/logout.saga.js
@@ -0,0 +1,50 @@
+// Core
+import { apply } from "redux-saga/effects";
+import { replace } from "react-router-redux";
+import { expectSaga } from 'redux-saga-test-plan';
+
+// Instruments
+import { api } from "../../../REST";
+import { authActions } from "../../auth/actions";
+import { uiActions } from "../../ui/actions";
+import { profileActions } from "../../profile/actions";
+import { postsActions } from "../../posts/actions";
+import { usersActions } from "../../users/actions";
+import { book } from '../../../navigation/book';
+import { actions } from "react-redux-form";
+import { logout } from "../saga/workers";
+
+describe('logout saga:', () => {
+    test('should complete a 204 status response scenario', async () => {
+        await expectSaga(logout)
+            .put(uiActions.startFetching())
+            .provide([[apply(api, api.auth.logout), __.fetchResponseSuccess204]])
+            .apply(localStorage, localStorage.removeItem, ['token'])
+            .apply(localStorage, localStorage.removeItem, ['remember'])
+            .put(profileActions.clearProfile())
+            .put(postsActions.clearPosts())
+            .put(usersActions.clearUsers())
+            .put(uiActions.stopFetching())
+            .put(actions.reset('forms.user'))
+            .put(authActions.logout())
+            .put(replace(book.login))
+            .run();
+    });
+    test('should complete a 401 status response scenario', async () => {
+        await expectSaga(logout)
+            .put(uiActions.startFetching())
+            .provide([[apply(api, api.auth.logout), __.fetchResponseFail401]])
+            .put(uiActions.emitError(__.error, "logout worker"))
+            .apply(localStorage, localStorage.removeItem, ['token'])
+            .apply(localStorage, localStorage.removeItem, ['remember'])
+            .put(profileActions.clearProfile())
+            .put(postsActions.clearPosts())
+            .put(usersActions.clearUsers())
+            .put(uiActions.stopFetching())
+            .put(actions.reset('forms.user'))
+            .put(authActions.logout())
+            .put(replace(book.login))
+            .run();
+    });
+
+});
